35483db9f24965bd4b91164c0c427362c8eecb2e
OTIENO VICTOR <otienov272@gmail.com>
2025-01-21 14:27:20 +0300

    Add files via upload

---
+++ password-gen.py
@@ -0,0 +1,16 @@
+#Generate a password with secrets and string
+
+import secrets 
+import string
+
+def generate_password(length=12 ):
+	alphabet = (
+		string.ascii_letters  +
+		string.digits +
+		string.punctuation
+	)
+	return ''.join(secrets.choice(alphabet)
+						for _ in range(length))
+
+password = generate_password()						
+print(f"Your new password:{password}")
